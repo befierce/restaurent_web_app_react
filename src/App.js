ef6555761bfd47ae7e81f593bd0102ef3f5d996f
befierce <enterbhatt123@gmail.com>
2023-11-21 17:08:47 +0530

    made hard coded cart overlay model

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
-    <div>
+    <>
+    <Cart></Cart>
       <Header />
+      
       <main>
         <Meals />
       </main>
-    </div>
+      
+    </>
   );
 }
 
